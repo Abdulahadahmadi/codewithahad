070519c101fa445fe1d7e9607a91c1ef8921243d
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2023-07-03 15:01:26 +0300

    add some changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,18 +6,18 @@ import { BsLinkedin, BsWhatsapp, BsInstagram, BsCodeSquare } from 'react-icons/b
 
 const Hero = () => {
   return (
-    <div className='flex flex-col-reverse md:flex-row justify-center items-center mt-10 md:mt-32 w-full'>
-      <section className='mr-60'>
+    <div className='flex flex-col-reverse md:flex-row justify-center items-center mt-10 lg:mt-40 w-full'>
+      <section className='lg:mr-60'>
         <h1 className='text-2xl md:text-3xl lg:text-6xl mx-5 md:mx-0  '>HEY! <strong>I'm Ahad,</strong> A <br /><strong>Software Developer <br /></strong>Based in <strong>Cyprus</strong></h1>
         <p className='text-gray-400'>Curious about learning new technologies,<br /> with experience of more than 2 years in this field.</p>
-        <div className='flex flex-row items-center'>
+        <div className='flex flex-row items-center justify-center lg:justify-start'>
           <Link className='text-black bg-gray-300 px-10 py-3 hover:bg-cyan-500 hover:text-white rounded-full' href='/contact' passHref>Contact Me</Link>
           <div className='flex m-4 justify-center items-center'>
-            <Link className='w-12 h-12 ring-1 rounded-full py-4 pl-4 ml-2 hover:bg-cyan-500' href='https://www.linkedin.com/in/ahad-ahmadi-1a7295175/' target='_blank' passHref>
+            <Link className='w-12 h-12 ring-1 rounded-full py-4 pl-4 ml-2 hover:bg-cyan-500 hover:text-white' href='https://www.linkedin.com/in/ahad-ahmadi-1a7295175/' target='_blank' passHref>
               <BsLinkedin />
             </Link>
-            <Link className='w-12 h-12 ring-1 rounded-full py-4 pl-4 ml-2 hover:bg-cyan-500' href='https://wa.me/35794448287' target='_blank' passHref><BsWhatsapp /></Link>
-            <Link className='w-12 h-12 ring-1 rounded-full py-4 pl-4 ml-2 hover:bg-cyan-500' href='https://www.instagram.com/ahad_ahmadi_01/' target='_blank' passHref><BsInstagram /></Link>
+            <Link className='w-12 h-12 ring-1 rounded-full py-4 pl-4 ml-2 hover:bg-cyan-500 hover:text-white' href='https://wa.me/35794448287' target='_blank' passHref><BsWhatsapp /></Link>
+            <Link className='w-12 h-12 ring-1 rounded-full py-4 pl-4 ml-2 hover:bg-cyan-500 hover:text-white' href='https://www.instagram.com/ahad_ahmadi_01/' target='_blank' passHref><BsInstagram /></Link>
           </div>
         </div>
         <span className='flex '><BsCodeSquare className='mt-1' /> &nbsp; UI Design-Software Development-App Development</span>
