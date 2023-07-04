c59858611c9bc3cf71956ef5238edd28d92a64d2
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2023-07-04 18:44:10 +0300

    add services to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Service from './components/Service'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Service />
     </div>
   )
 }
