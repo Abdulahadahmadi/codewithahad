cd70f48f034cd8adecee4f6c339a46b4cc500350
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2023-07-15 16:57:55 +0300

    implement component

---
+++ src/app/components/Portfilio.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div className='w-full bg-gray-200 py-10'>
+      <h1 className='text-xl md:text-2xl lg:text-4xl text-cyan-500 text-center'>Portfolio</h1>
+      <p className='text-2xl md:text-4xl lg:text-6xl text-center font-bold my-8'>Digital Product Showcases</p>
+      
+    </div>
+  )
+}
+
+export default Portfolio
