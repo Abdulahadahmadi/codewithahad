ef39fb9fd8960151ce5ea57cbfb5ccd02e0fbca9
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-07-30 15:53:15 -0700

    add some changes

--- src/app/components/Service.tsx
+++ src/app/components/Service.tsx
@@ -11,7 +11,7 @@ const Service = () => {
         <span className='text-cyan-500 text-center text-2xl md:text-5xl font-bold'>Service</span>
         <h1 className='text-2xl md:text-3xl font-bold mx-5'>Expertise Service! Let's check it out</h1>
         <p className='text-gray-800 mx-5 md:mx-16 lg:mx-80 justify-start'>Meet <strong>Ahad</strong>, your dedicated software developer and technology enthusiast. With a passion for creating innovative solutions, I specialize in custom software development, turning your ideas into reality with precision and expertise. Let's collaborate to bring your projects to life and make a meaningful impact.</p>
-      <div className='grid lg:grid-cols-3 md:gap-5 mx-10 md:ml-28'>
+      <div className='grid lg:grid-cols-3 md:gap-5 mx-10 '>
         
         <section className='bg-gray-100 w-72 h-72 md:w-96 md:h-96 rounded-xl shadow-xl p-10 md:p-16 leading-3 md:mx-20 my-7 lg:my-16 mx-auto hover:bg-cyan-500 cursor-pointer hover:rotate-6'>
           <div className='flex justify-center items-center mb-8'>
