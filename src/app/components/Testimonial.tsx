3a22d29ed591bdc486266e39a3c221245e9effb8
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-07-31 18:18:01 +0300

    fix the buttons positions in mobile mode

--- src/app/components/Testimonial.tsx
+++ src/app/components/Testimonial.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Testimonial = () => {
   const [data, setData] = useState([]);
@@ -17,57 +18,61 @@ const Testimonial = () => {
         What People Say's About Me
       </p>
       <div className="flex flex-col md:flex-row justify-center">
-        <section className="w-64 h-64 md:w-96 md:h-80 bg-white md:m-10 rounded-xl shadow-xl   md:p-8 hover:bg-cyan-500 hover:decoration-gray-100 hover:cursor-pointer rotate-12 animate-pulse">
-          <p>
-            Abdul Ahad Ahmadi is a prestigious member of any team as he always
-            dedicates all into work and I really like how he's knows about
-            bleeding-edge technologies. He always works so hard and comes with
-            best-practices about front-end. I recommend him to anybody who likes
-            a highly-skilled employee.
-          </p>
-          <div className="flex flex-row justify-between mt-2">
-            <Image
-              src="https://media.licdn.com/dms/image/D4D03AQHJjt2dMbzdYA/profile-displayphoto-shrink_200_200/0/1676427445015?e=1695859200&v=beta&t=-OHzEUNx-8dmdwB5sgHzP0kBeKdF2YqWa0D16gpKPBE"
-              alt="profile"
-              width={50}
-              height={50}
-              className="object-cover object-center rounded-full"
-            />
-            <div className="flex flex-col">
-              <span className="text-lg font-bold">Ali Gulzar</span>
-              <span className="text-gray-600">
-                Full Stack Software Developer
-              </span>
+        <Link target="_blank" passHref href='https://www.linkedin.com/in/ahad-ahmadi-1a7295175/'>
+          <section className="md:max-w-md  bg-white m-4 md:m-10 rounded-xl shadow-xl p-6 md:p-8 hover:bg-cyan-500 hover:text-white hover:decoration-gray-100 hover:cursor-pointer md:rotate-12 mt-6">
+            <p className="text-sm md:text-md ">
+              Abdul Ahad Ahmadi is a prestigious member of any team as he always
+              dedicates all into work and I really like how he's knows about
+              bleeding-edge technologies. He always works so hard and comes with
+              best-practices about front-end. I recommend him to anybody who likes
+              a highly-skilled employee.
+            </p>
+            <div className="flex flex-row justify-between mt-2">
+              <Image
+                src="https://media.licdn.com/dms/image/D4D03AQHJjt2dMbzdYA/profile-displayphoto-shrink_200_200/0/1676427445015?e=1695859200&v=beta&t=-OHzEUNx-8dmdwB5sgHzP0kBeKdF2YqWa0D16gpKPBE"
+                alt="profile"
+                width={70}
+                height={70}
+                className="object-cover object-center rounded-full"
+              />
+              <div className="flex flex-col">
+                <span className="text-md md:text-lg font-bold">Ali Gulzar</span>
+                <span className="text-xs md:text-md">
+                  Full Stack Software Developer
+                </span>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </Link>
 
-        <section className="w-80 h-64 md:w-96 md:h-96 bg-white m-10 rounded-xl  shadow-xl p-8 hover:bg-cyan-500 hover:decoration-gray-100 hover:cursor-pointer rotate-12 animate-pulse">
-          <p>
-            Abdul Ahad Ahmadi is a passionate and hardworking programmer with a
-            creative mind. Abdul Ahad has helped me on many projects and it was
-            very helpful to have him around. He is the go-to resource for me
-            whenever I have any issue on my projects. I really appreciate his
-            help and being such a friendly person. As someone who has helped me
-            a lot not only with my projects but about the sources and paths to
-            take to improve my programming skills, I hardly recommend him.
-          </p>
-          <div className="flex flex-row justify-around mt-2">
-            <Image
-              src="https://media.licdn.com/dms/image/C5603AQFOa7qV9dMmsg/profile-displayphoto-shrink_200_200/0/1652988970949?e=1695859200&v=beta&t=yPhoVfWZNME_opJtedZy8jOPP4uaOsYxpjjyU3HQq0I"
-              alt="profile"
-              width={50}
-              height={30}
-              className="object-cover object-center rounded-full mr-6"
-            />
-            <div className="flex flex-col">
-              <span className="text-lg font-bold">Khudadad Khawari</span>
-              <span className="text-gray-600">
-                Software Developer | ML & Data Science enthusiast
-              </span>
+        <Link target="_blank" passHref href='https://www.linkedin.com/in/ahad-ahmadi-1a7295175/'>  
+          <section className="md:max-w-md   bg-white m-4 md:m-10 rounded-xl shadow-xl p-6 md:p-8 hover:bg-cyan-500  hover:text-white hover:decoration-gray-100 hover:cursor-pointer md:rotate-12 ">
+            <p className="text-sm md:text-md ">
+              Abdul Ahad Ahmadi is a passionate and hardworking programmer with a
+              creative mind. Abdul Ahad has helped me on many projects and it was
+              very helpful to have him around. He is the go-to resource for me
+              whenever I have any issue on my projects. I really appreciate his
+              help and being such a friendly person. As someone who has helped me
+              a lot not only with my projects but about the sources and paths to
+              take to improve my programming skills, I hardly recommend him.
+            </p>
+            <div className="flex flex-row justify-between mt-2">
+              <Image
+                src="https://media.licdn.com/dms/image/C5603AQFOa7qV9dMmsg/profile-displayphoto-shrink_200_200/0/1652988970949?e=1695859200&v=beta&t=yPhoVfWZNME_opJtedZy8jOPP4uaOsYxpjjyU3HQq0I"
+                alt="profile"
+                width={70}
+                height={70}
+                className="object-cover object-center rounded-full mr-6"
+              />
+              <div className="flex flex-col">
+                <span className="text-md md:text-lg font-bold">Khudadad Khawari</span>
+                <span className="text-xs md:text-md">
+                  Software Developer | ML & Data Science enthusiast
+                </span>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </Link>
 
       </div>
     </div>
