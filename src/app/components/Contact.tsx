d869fe53c5e1d0f2ca92ba4df1bfccc063ada1e0
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-08-03 13:43:42 +0300

    style chane

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import discuss from '../../../public/discuss.png'
 
 const Contact = () => {
   return (
-    <div className='flex flex-col justify-center md:justify-between items-center bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full p-10 mx:p-20'>
-      <h1 className='text-xl md:text-3xl lg:text-5xl   font-bold'>Got a Project! Let's Talk</h1>
-      <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rem beatae tempora harum sunt? Atque ut qui, adipisci a porro nesciunt sed asperiores nobis, mollitia omnis doloremque quod eius culpa?</p>
-      <Link href='/contact' passHref className='text-black bg-gray-300 px-6 md:px-10 py-2 md:py-3 hover:bg-cyan-500 hover:text-white rounded-full'>
-          Contact
-      </Link>
+    <div className='flex flex-col md:flex-row md:justify-around items-center bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full p-10 mx:p-20'>
+      <section>
+        <Image width={500} height={450} src={discuss} alt='group discuss' />
+      </section>
+      
+      <section className=' md:leading-8'>
+        <h1 className='text-xl md:text-3xl lg:text-5xl text-white font-bold'>Got a Project! Let's Talk</h1>
+        <p className='text-white mb-4'>Have an exciting project idea in mind? Let's chat and make it happen together!</p>
+        <Link href='/contact' passHref className='text-black bg-gray-300 px-6 md:px-10 py-2 md:py-3 hover:bg-cyan-500 hover:text-white rounded-full'>
+            Contact
+        </Link>
+      </section>
     </div>
   )
 }
