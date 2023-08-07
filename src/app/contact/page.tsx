53c6dbced73175e52c1fafbc908e742ed1e4fb85
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-08-07 18:52:43 +0300

    add contact form to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ContactForm from '../components/ContactForm'
 
-const page = () => {
+const page: React.FC = () => {
+  const style = {
+    background:'rgba( 255, 255, 255, 0.25 )',
+    backdropFilter: 'blur( 4px )',
+    webkitBackdropFilter: 'blur( 4px )',
+    borderRadius: '10px',
+    border: '1px solid rgba( 255, 255, 255, 0.18 )'
+  }
   return (
-    <div>Contact Me</div>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <ContactForm />
+    </div>
   )
 }
 
