47acb1e65245e0ef267ce515b4375ff415172fc7
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-08-21 15:50:36 +0300

    add category section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,18 +11,21 @@ const page = () => {
       description: 'This is the description for Project 1.',
       image: 'https://media.licdn.com/dms/image/C4D22AQFKiDlJUoUT2g/feedshare-shrink_800/0/1674668981057?e=1694649600&v=beta&t=shJFYPBeyyZAEOW_ufwnGx_F2ipIkZeEsBqfBXlprGM',
       link: 'https://jobs.af',
+      category: 'saas product',
     },
     {
       title: 'Project 2',
       description: 'This is the description for Project 2.',
       image: 'https://github.com/Abdulahadahmadi/admin-dashboard/raw/main/public/assets/screenshots/pic-1.png',
       link: 'https://jobs.af',
+      category: 'dashboard',
     },
     {
       title: 'Project 3',
       description: 'This is the description for Project 2.',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3Q54FgNW2x2Rp9oY70DjRT6NDlCHomynOt2NyRvpow&s',
       link: 'https://jobs.af',
+      category: 'landing page',
     },
     
     // Add more projects here
@@ -30,14 +33,8 @@ const page = () => {
 
   return (
     <div className='w-full py-6'>
-      <div className='flex flex-row-reverse justify-end items-end gap-2 ring-1 rounded-md px-2 py-1 w-36 md:w-40 hover:cursor-pointer hover:bg-cyan-500 hover:text-white'>
-          <Link href='/' passHref className='flex flex-row justify-center items-center gap-1 md:gap-3'>
-          <span>back to home</span>
-              <BiArrowBack />
-          </Link>
-      </div>
-      <h1 className='text-xl md:text-2xl lg:text-4xl text-cyan-500 text-center'>Portfolio</h1>
-      <p className='text-2xl md:text-4xl lg:text-6xl text-center font-bold my-8'>Product Showcases</p>
+      {/* <h1 className='text-xl md:text-2xl lg:text-4xl text-cyan-500 text-center'>Portfolio</h1> */}
+      <p className='text-2xl md:text-4xl lg:text-5xl text-center font-bold my-8'>Products Showcases</p>
       <section className='flex flex-row justify-center gap-2 md:gap-4'>
         <button className='p-1 md:px-4 md:py-2 hover:text-white hover:bg-cyan-500 ring-1 text-xs md:text-base rounded mx-1'>All</button>
         <button className='p-1 md:px-4 md:py-2 hover:text-white hover:bg-cyan-500 ring-1 text-xs md:text-base rounded mx-1'>Landing Page</button>
