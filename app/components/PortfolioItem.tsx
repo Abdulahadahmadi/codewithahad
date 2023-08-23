b90e1962d898b09635ed3ef621f51d856de61fdb
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-08-23 17:52:22 +0300

    fix style

--- app/components/PortfolioItem.tsx
+++ app/components/PortfolioItem.tsx
@@ -26,12 +26,12 @@ const PortfolioItem: React.FC<{ project: Project }> = ({ project }) => {
       onMouseLeave={handleMouseLeave}
     >
       <div
-        className="h-80 w-full rounded-md overflow-hidden cursor-pointer transition duration-500 hover:opacity-70 bg-no-repeat shadow-xl bg-cover mt-2 md:mt-4"
+        className="h-80 w-full rounded-md overflow-hidden cursor-pointer transition hover:bg-opacity-70 duration-500 bg-no-repeat shadow-xl bg-cover mt-2 md:mt-4"
         style={{ backgroundImage: `url(${project.image})` }}
       />
       
       {isHovered && (
-        <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center rounded-md">
+        <div className="absolute inset-0 mt-4 bg-black bg-opacity-70 flex items-center justify-center rounded-md">
           <div className="text-white text-center">
             <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
             <p className="mb-4">{project.description}</p>
