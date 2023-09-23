6390b9a8357bb39c66ae2785a74409a3e4fa560d
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-09-23 18:15:52 +0300

    add google adsense component

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,14 @@ import Testimonial from './components/Testimonial'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import GoogleAdSense from './components/GoogleAdSense'
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Service />
+      <GoogleAdSense />
       <About />
       <Testimonial />
       <Portfolio />
