f44f8dd38b477422d45185eabf2139a57b2e6687
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-09-24 17:19:47 +0300

    add meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,18 @@ import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import GoogleAdSense from './components/GoogleAdSense'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
+      {/* write head section for seo of my website */}
+      <Head>
+        <title>Code With Ahad</title>
+        <meta name="description" content="Explore a world of programming and tech insights on 'Code with Ahad.' Discover in-depth articles, tutorials, and posts covering software development, coding tips, and the latest tech trends. Level up your skills and stay up-to-date with Ahad's expertise in the world of technology." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Hero />
       <Service />
       <GoogleAdSense />
