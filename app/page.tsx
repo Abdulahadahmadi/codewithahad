56ef41fed5abc0fe0c928524ef5f18878593fa00
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-09-27 01:23:53 +0300

    add keywords

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <Head>
         <title>Code With Ahad</title>
         <meta name="description" content="Explore a world of programming and tech insights on 'Code with Ahad.' Discover in-depth articles, tutorials, and posts covering software development, coding tips, and the latest tech trends. Level up your skills and stay up-to-date with Ahad's expertise in the world of technology." />
-        <meta name='keywords' content='code with ahad, codewithahad, programming, coding, blogging, Abdul Ahad, Ahad, Technology personal platform, blog, code with ahad in youtube, software, software developer, development, ui, ux, design, ahad design' />
+        <meta name='keywords' content='code with ahad, codewithahad, web, website, web app, web application, programming, coding, blogging, Abdul Ahad, Ahad, Technology personal platform, blog, code with ahad in youtube, software, software developer, development, ui, ux, design, ahad design, youtube channel, youtube, problem solving, programming competition, icpc, international colligate programming contest, acm' />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
