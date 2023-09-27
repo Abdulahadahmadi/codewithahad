d5b9c8e77e5990051d341ff84e1cc4f014540cc8
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-09-27 16:47:30 +0300

    add anim to sections

--- app/components/Service.tsx
+++ app/components/Service.tsx
@@ -1,33 +1,48 @@
-import React from 'react'
-import Card from './Card'
+'use client'
+
+import React, { useEffect } from 'react'
 import { GiOrange } from 'react-icons/gi'
 import { GrAppsRounded } from 'react-icons/gr'
 import {  BsCodeSlash } from 'react-icons/bs'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const Service = () => {
+  
+  useEffect(() => {
+    AOS.init({
+      duration: 1000
+    });
+  }, []);
+
+
+
   return (
     <div className='mx-auto'>
-      <div className='text-center my-10 '>
+      <div className='text-center my-10' data-aos="fade-right" data-aos-duration="500">
         <h1 className='text-cyan-500 text-center text-2xl md:text-5xl font-bold'>Service</h1>
         <p className='text-2xl md:text-3xl font-bold mx-5'>Expertise Service! Let's check it out</p>
         <p className='mx-5 md:mx-16 lg:mx-80 justify-start'>Meet <strong>Ahad</strong>, your dedicated software developer and technology enthusiast. With a passion for creating innovative solutions, I specialize in custom software development, turning your ideas into reality with precision and expertise. Let's collaborate to bring your projects to life and make a meaningful impact.</p>
       <div className='grid lg:grid-cols-3 md:gap-5 mx-10 '>
         
-        <section className='w-72 h-72 md:w-96 md:h-96 rounded-xl shadow-xl p-10 md:p-16 leading-3 md:mx-20 my-7 lg:my-16 mx-auto hover:bg-cyan-500 cursor-pointer hover:rotate-6'>
+        <section className='w-72 h-72 md:w-96 md:h-96 rounded-xl shadow-xl p-10 md:p-16 leading-3 md:mx-20 my-7 lg:my-16 mx-auto hover:bg-cyan-500 cursor-pointer hover:rotate-6' data-aos="flip-right">
           <div className='flex justify-center items-center mb-8'>
             <GiOrange />
           </div>
           <p className='font-bold text-2xl md:text-3xl'>UI & UI Design</p>
           <p className='text-center leading-5 mt-3'>Designing with Purpose Inspiring Engagement Unleash the Full Potential of Your Product with Our UI/UX Expertise</p>
         </section>
-        <section className='w-72 h-72 md:w-96 md:h-96 rounded-xl shadow-xl p-10 md:p-16 leading-3 md:mx-20 my-7 lg:my-16 mx-auto hover:bg-cyan-500 cursor-pointer hover:rotate-6'>
+
+        <section className='w-72 h-72 md:w-96 md:h-96 rounded-xl shadow-xl p-10 md:p-16 leading-3 md:mx-20 my-7 lg:my-16 mx-auto hover:bg-cyan-500 cursor-pointer hover:rotate-6' data-aos="flip-right">
           <div className='flex justify-center items-center mb-8'>
             <BsCodeSlash />
           </div>
           <p className='font-bold text-2xl md:text-3xl'>Software Development</p>
           <p className='text-center leading-5 mt-3'>Transforming Vision into Reality Our Software Development Service Delivers Custom Solutions for Your Unique Needs</p>
         </section>
-        <section className='w-72 h-72 md:w-96 md:h-96 rounded-xl shadow-xl p-10 md:p-16 leading-3 md:mx-20 my-7 lg:my-16 mx-auto hover:bg-cyan-500 cursor-pointer hover:rotate-6'>
+
+        <section className='w-72 h-72 md:w-96 md:h-96 rounded-xl shadow-xl p-10 md:p-16 leading-3 md:mx-20 my-7 lg:my-16 mx-auto hover:bg-cyan-500 cursor-pointer hover:rotate-6' data-aos="flip-right">
           <div className='flex justify-center items-center mb-8'>
             <GrAppsRounded />
           </div>
