5321b049d0a862dbb06c1e90a279b79f1a7308b6
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-09-27 17:30:21 +0300

    add anim

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -37,69 +37,69 @@ const About = () => {
 
 
       <p className='text-cyan-500 text-2xl md:text-5xl font-bold leading-6 my-8'>Sklls</p>
-      <section className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3 md:gap-5' data-aos="fade-up-left" data-aos-duration="500">
-        <div title='html' className='skills-cyan'>
+      <section className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3 md:gap-5' >
+        <div title='html' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="100">
           <Image alt='html' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
         </div>
         
-        <div title='css' className='skills-cyan'>
+        <div title='css' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="300">
           <Image alt='css' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
         </div>
             
-        <div title='sass' className='skills-cyan'>
+        <div title='sass' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="500">
           <Image alt='sass' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
         </div>
           
-        <div title='tailwind css' className='skills-black'>
+        <div title='tailwind css' className='skills-black' data-aos="fade-up-left" data-aos-duration="700">
           <Image alt='tailwindcss' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
         </div>
 
 
-        <div title='javascript' className='skills-cyan'>
+        <div title='javascript' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="900">
           <Image alt='JavaScript' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
         </div>
           
-        <div title='typescript' className='skills-cyan'>
+        <div title='typescript' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="600">
           <Image alt='TypeScript' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
         </div>
 
-        <div title='react' className='skills-black'>
+        <div title='react' className='skills-black' data-aos="fade-up-left" data-aos-duration="700">
           <Image alt='React.js' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
         </div>
 
-        <div title='nextjs' className='skills-cyan'>
+        <div title='nextjs' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="1100">
           <Image alt='Next.js' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" />
         </div>
 
-        <div title='vuejs' className='skills-cyan'>
+        <div title='vuejs' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="1300">
           <Image alt='Vue.js' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" />
         </div>
   
-        <div title='nuxtjs' className='skills-cyan'>
+        <div title='nuxtjs' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="1500">
           <Image alt='Nuxt.js' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-plain-wordmark.svg" />
         </div>
 
-        <div title='nodejs' className='skills-cyan'>
+        <div title='nodejs' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="1700">
           <Image alt='Node' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
         </div>
           
-        <div title='nodejs' className='skills-cyan'>
+        <div title='nodejs' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="1800">
           <Image alt='Node' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
         </div>
           
 
-        <div title='git' className='skills-cyan'>
+        <div title='git' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="1900">
           <Image alt='git' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
         </div>
           
 
-        <div title='github' className='skills-cyan'>
+        <div title='github' className='skills-cyan' data-aos="fade-up-left" data-aos-duration="2000">
           <Image alt='github' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
         </div>
           
           
 
-        <div title='docker' className='skills-black'>
+        <div title='docker' className='skills-black' data-aos="fade-up-left" data-aos-duration="2100">
           <Image alt='docker' width={80} height={80} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg" />
         </div>
           
