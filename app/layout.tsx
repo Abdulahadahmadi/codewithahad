135b8f6c23fc700138b449990daca7020f33f662
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-09-29 00:22:53 +0300

    remove useless code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.scss'
 import Navbar from './components/Navbar'
 import { Inter } from 'next/font/google'
 import Footer from './components/Footer'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
