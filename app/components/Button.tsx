470d851d8139371c8868a6c46545e34dc079908c
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-10-02 00:39:34 +0300

    change cololr

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -11,7 +11,7 @@ const Button = ({ text }: Props) => {
 
   return (
     <div>
-      <button onClick={addText} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md w-full">
+      <button onClick={addText} className="bg-cyan-500 hover:bg-cyan-700 text-white shadow-lg font-bold py-2 px-4 rounded-md w-full">
         {text}
       </button>
     </div>
