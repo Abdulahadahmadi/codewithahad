9b0ef70ae8430db2836c239ee8610cf6147d65c5
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-10-04 12:40:18 +0300

    remove extra code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.scss'
 import Navbar from './components/Navbar'
 import { Inter } from 'next/font/google'
-import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
