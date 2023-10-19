2b951c36c81f3daec1f781065bd291ac6561a486
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-10-19 13:26:40 +0300

    add Login form component to login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import LoginForm from '@/app/components/blog/LoginForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <LoginForm
+            userName=''
+            email=''
+            password=''
+        />
+    </div>
+  )
+}
+
+export default page
