3fb4a90b463b7de24f30d187623446d4fdfd2ba0
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-03 12:34:04 +0200

    implement new font

---
+++ app/font.ts
@@ -0,0 +1,5 @@
+import { Inter, Edu_TAS_Beginner } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const edu_tas_beginner  = Edu_TAS_Beginner({ weight: ['400', '700'], subsets: ['latin']})
