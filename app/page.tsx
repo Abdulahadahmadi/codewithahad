87e7e5ab6eddbe59ba5c0eeddd36e0afd0697772
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-10 13:41:48 +0200

    add meta data for root page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,14 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 import GoogleAdSense from './components/GoogleAdSense'
 import Head from 'next/head'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Code With Ahad',
+  description: 'Explore a world of programming and tech insights on "Code with Ahad." Discover in-depth articles, tutorials, and posts covering software development, coding tips, and the latest tech trends. Level up your skills and stay up-to-date with Ahad\'s expertise in the world of technology.',
+  keywords: 'code with ahad, codewithahad, web, website, web app, web application, programming, coding, blogging, Abdul Ahad, Ahad, Technology personal platform, blog, code with ahad in youtube, software, software developer, development, ui, ux, design, ahad design, youtube channel, youtube, problem solving, programming competition, icpc, international colligate programming contest, acm',
+  metadataBase: new URL('https://codewithahad.com/'),
+}
 
 export default function Home() {
   return (
