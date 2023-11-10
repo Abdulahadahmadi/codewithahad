e39ddff43eb8202de6d115355d315f310b147af7
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-10 13:41:35 +0200

    add meta date options to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,27 @@
 'use client'
 
 import './globals.scss'
-import Navbar from './components/Navbar'
 import { Inter } from 'next/font/google'
-import { useCurrentRoute } from '@/hooks/useCurrentRoute'
 import ScrollToTopButton from './components/ScrollToTopButton'
 import { edu_tas_beginner } from './font'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const metadata: Metadata = {
+  title: {
+    template: '%s | Code With Ahad',
+    default: 'Code With Ahad',
+  },
+  description: 'A blog about web development and programming',
+  metadataBase: new URL('https://codewithahad.com/'),
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const currentRoute = useCurrentRoute();
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={edu_tas_beginner.className}>
