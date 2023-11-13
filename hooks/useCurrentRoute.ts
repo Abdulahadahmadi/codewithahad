2a3481bb3eab56b564012ccf618d9333d00aa20f
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-13 23:07:22 +0200

    remove file

--- hooks/useCurrentRoute.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const RouteContext = createContext('');
-
-export const RouterProvider = RouteContext.Provider;
-
-export const useCurrentRoute = () => {
-    return useContext(RouteContext);
-}
