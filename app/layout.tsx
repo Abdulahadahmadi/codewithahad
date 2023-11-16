26f2072c8706edbdac91ea22807ba3075bdba39d
Abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2023-11-16 17:14:48 +0200

    remove the mouse shadow from mobile mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
     <html lang="en">
       <body suppressHydrationWarning={true} className={edu_tas_beginner.className}>
         {children}
-        <MouseShadow />
+        <div className='hidden md:block'>
+          <MouseShadow />
+        </div>
         <div className='sr-only md:not-sr-only'>
           <ScrollToTopButton />
         </div>
